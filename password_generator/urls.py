a171c62c44a98d591fe5d94c8dc3957ac00e68a7
pythonbalm <78608269+pythonbalm@users.noreply.github.com>
2021-05-07 22:37:50 +0400

    first_commit

---
+++ password_generator/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from generator import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('password', views.password, name='password')
+]
